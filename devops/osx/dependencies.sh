e8dd6aaf9c7b9992f1cd3ac4e75c254ace9e8746
github.user-at-576386355408 <bcooke91@gmail.com>
2018-10-21 20:05:08 -0400

    Maybe fixing the vbscript

---
+++ devops/osx/dependencies.sh
@@ -0,0 +1,5 @@
+#install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+#install docker
+brew install docker docker-compose docker-machine xhyve docker-machine-driver-xhyve
